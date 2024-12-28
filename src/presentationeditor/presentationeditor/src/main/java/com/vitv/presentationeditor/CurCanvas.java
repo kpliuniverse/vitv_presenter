8f9a728a8b613efa784dfe39663b538d5bac7a5a
kpliuniverse <lolitalaroasas@gmail.com>
2024-12-28 21:13:44 +0800

    java stuff

---
+++ src/presentationeditor/presentationeditor/src/main/java/com/vitv/presentationeditor/CurCanvas.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vitv.presentationeditor;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author kpliuniverse
+ */
+public class CurCanvas {
+
+    // The private static instance of the class (initialized to null).
+    private static CurCanvas instance;
+
+    
+    //vars and stuff
+    public JPanel panel;
+    // Private constructor to prevent instantiation from outside the class.
+    private CurCanvas() {
+        this.panel = new JPanel();
+        this.panel.setBackground(Color.white);
+        this.panel.setPreferredSize(new Dimension(1500, 1500));
+        
+    }
+
+    // Public method to access the instance.
+    public static CurCanvas getInstance() {
+        if (instance == null) {
+            instance = new CurCanvas(); // Create the instance only when it's needed
+        }
+        return instance;
+    }
+
+    // Example method
+    public void doSomething() {
+        System.out.println("Canvas is doing something.");
+    }
+}
---
+++ src/presentationeditor/presentationeditor/src/main/java/com/vitv/presentationeditor/Presentationeditor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vitv.presentationeditor;
+
+/**
+ *
+ * @author kpliuniverse
+ */
+public class Presentationeditor {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
