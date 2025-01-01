d68b85938b7206a94e93d08ae3e52dab01bd5b09
kpliuniverse <felixsas23@gmail.com>
2025-01-01 15:27:25 +0800

    vitvpresenter via raylib

---
+++ src/presrender/src/main.c
@@ -0,0 +1,49 @@
+/*
+Raylib example file.
+This is an example main file for a simple raylib project.
+Use this as a starting point or replace it with your code.
+
+by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+
+*/
+
+#include "raylib.h"
+
+#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+int main ()
+{
+	// Tell the window to use vsync and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+	// Create the window and OpenGL context
+	InitWindow(1280, 800, "Test");
+
+	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	SearchAndSetResourceDir("labrat/in");
+	
+	// game loop
+	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	{
+		// drawing
+		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
+		ClearBackground(BLACK);
+
+		// draw some text using the default font
+		DrawText("kaitentou kara nai gengo\nhaitenshon to katai tenpo\ni sendou suru jaiken go\nkara daikenkai senhou!", 200,200,20,WHITE);
+
+		
+		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		EndDrawing();
+	}
+
+	// cleanup
+	// unload our texture so it can be cleaned up
+	UnloadTexture(wabbit);
+
+	// destroy the window and cleanup the OpenGL context
+	CloseWindow();
+	return 0;
+}
